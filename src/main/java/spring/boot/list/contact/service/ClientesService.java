25ac45b9dab3a91e34f6443b8ba9323367aed100
Zenx007 <zenxpokas@gmail.com>
2025-04-29 23:56:32 -0300

    fix: clientes service

---
+++ src/main/java/spring/boot/list/contact/service/ClientesService.java
@@ -0,0 +1,74 @@
+package spring.boot.list.contact.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import spring.boot.list.contact.dto.ClientesDTO;
+import spring.boot.list.contact.dto.ClientesResponseDTO;
+import spring.boot.list.contact.dto.ContatoResponseDTO;
+import spring.boot.list.contact.model.Clientes;
+import spring.boot.list.contact.model.Contato;
+import spring.boot.list.contact.repository.ClientesRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ClientesService {
+
+    @Autowired
+    private ClientesRepository clientesRepository;
+
+    public Clientes salvarCliente(ClientesDTO dto) {
+
+        Clientes clientes = new Clientes();
+        clientes.setNome(dto.getNome());
+
+        if (dto.getContatos() != null && dto.getContatos().size() > 0) {
+            List<Contato> contatos = dto.getContatos().stream().map(c -> {
+                Contato contato = new Contato();
+                contato.setTelefone(c.getTelefone());
+                contato.setEmail(c.getEmail());
+                contato.setClientes(clientes);
+                return contato;
+            }).collect(Collectors.toList());
+            clientes.setContatos(contatos);
+        }
+        return clientesRepository.save(clientes);
+    }
+
+    public List<ClientesResponseDTO> listarTodos() {
+        return clientesRepository.findAll().stream().map(this::toDTO).collect(Collectors.toList());
+    }
+
+    public List<ContatoResponseDTO> listarContatosPorCliente(Long clienteId) {
+
+        Clientes cliente = clientesRepository.findById(clienteId)
+                .orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+
+        return cliente.getContatos().stream().map(c -> {
+            ContatoResponseDTO dto = new ContatoResponseDTO();
+            dto.setId(c.getId());
+            dto.setTelefone(c.getTelefone());
+            dto.setEmail(c.getEmail());
+            return dto;
+
+        }).collect(Collectors.toList());
+    }
+
+    private ClientesResponseDTO toDTO(Clientes cliente) {
+        ClientesResponseDTO dto = new ClientesResponseDTO();
+        dto.setId(cliente.getId());
+        dto.setNome(cliente.getNome());
+
+        List<ContatoResponseDTO> contatos = cliente.getContatos().stream().map(c -> {
+            ContatoResponseDTO contatoDTO = new ContatoResponseDTO();
+            contatoDTO.setId(c.getId());
+            contatoDTO.setTelefone(c.getTelefone());
+            contatoDTO.setEmail(c.getEmail());
+            return contatoDTO;
+        }).collect(Collectors.toList());
+        dto.setContatos(contatos);
+
+
+        return dto;
+
+    }
+}
