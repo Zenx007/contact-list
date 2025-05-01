ab58830a47a99dfa3beb35d283cb57f96c55cf41
Zenx007 <zenxpokas@gmail.com>
2025-04-30 22:51:02 -0300

    feat: GetMapping listar Todos

--- src/main/java/spring/boot/list/contact/controller/ClienteController.java
+++ src/main/java/spring/boot/list/contact/controller/ClienteController.java
@@ -1,14 +1,15 @@
 package spring.boot.list.contact.controller;
 
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import spring.boot.list.contact.dto.ClientesResponseDTO;
 import spring.boot.list.contact.model.Clientes;
 import spring.boot.list.contact.service.ClientesService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/clientes")
@@ -17,10 +18,17 @@ public class ClienteController {
     @Autowired
     private ClientesService clientesService;
 
-    @PostMapping("path")
+    @PostMapping
     public String criar (@RequestBody String entity) {
 
         Clientes clienteSalva = ClientesService.salvarCliente(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(clienteSalva);
     }
+
+    @GetMapping
+    public ResponseEntity<List<ClientesResponseDTO>>listarTodos(){
+        return ResponseEntity.ok(clientesService.listarTodos());
+
+    }
+
 }
