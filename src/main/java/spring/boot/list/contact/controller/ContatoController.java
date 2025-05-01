c6f70611625c63b4a678d87bd4dee67deb1fe00b
Zenx007 <zenxpokas@gmail.com>
2025-04-30 23:04:21 -0300

    feat: Post criar contato

--- src/main/java/spring/boot/list/contact/controller/ContatoController.java
+++ src/main/java/spring/boot/list/contact/controller/ContatoController.java
@@ -1,10 +1,44 @@
 package spring.boot.list.contact.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import spring.boot.list.contact.dto.ContatoDTO;
+import spring.boot.list.contact.model.Clientes;
+import spring.boot.list.contact.model.Contato;
+import spring.boot.list.contact.repository.ClientesRepository;
+import spring.boot.list.contact.repository.ContatoRepository;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/contatos")
 public class ContatoController {
+
+    @Autowired
+    private ContatoRepository contatoRepository;
+
+    @Autowired
+    private ClientesRepository clientesRepository;
+
+    @PostMapping
+    public ResponseEntity<?> criar(@RequestBody ContatoDTO dto) {
+
+        Optional <Clientes> clientesOpt= clientesRepository.findById(dto.getClienteId);
+        if (clientesOpt.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Cliente não encontrado");
+        }
+
+        Contato contato = new Contato();
+        contato.setTelefone(dto.getTelefone());
+        contato.setEmail(dto.getEmail());
+        contato.setClientes(clientesOpt.get());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(contato);
+    }
 
 }
