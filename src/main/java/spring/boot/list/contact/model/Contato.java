a1cd86cc7932957fe60f0a0fc4e923fc14a7921a
Zenx007 <zenxpokas@gmail.com>
2025-05-01 23:57:10 -0300

    feat: Update Contato

--- src/main/java/spring/boot/list/contact/model/Contato.java
+++ src/main/java/spring/boot/list/contact/model/Contato.java
@@ -18,8 +18,10 @@ public class Contato {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String telefone;
 
+    @Column(nullable = false)
     private String email;
 
     @ManyToOne
